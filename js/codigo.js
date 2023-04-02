2acbe48575743caf08d8ce17caaf40a673361fb7
Diego Anderson Cabrera <anderson_dc17@hotmail.com>
2023-04-02 15:40:25 -0500

    mejora verbos1 js

--- js/codigo.js
+++ js/codigo.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
   var verbos = ['Arise', 'Awake', 'Be', 'Beat', 'Become', 'Begin', 'Bet', 'Bite', 'Blow', 'Break', 'Bring', 'Build', 'Buy', 'Catch', 'Choose', 'Come', 'Cost', 'Cut', 'Dig', 'Do', 'Draw', 'Dream', 'Drink', 'Drive', 'Eat', 'Fall', 'Feed', 'Feel', 'Fight', 'Find', 'Fly', 'Forget', 'Forgive', 'Freeze', 'Get', 'Give', 'Go', 'Grow', 'Have', 'Hear', 'Hide', 'Hit', 'Hurt', 'Keep', 'Kneel', 'Know', 'Lean', 'Learn', 'Leave', 'Lend', 'Let', 'Lie', 'Light', 'Lose', 'Make', 'Mean', 'Meet', 'Pay', 'Plead', 'Put', 'Quit', 'Read', 'Ride', 'Rise', 'Run', 'Say', 'See', 'Seek', 'Sell', 'Send', 'Set', 'Shake', 'Shine', 'Shoot', 'Shut', 'Sing', 'Sleep', 'Smell', 'Speak', 'Spell', 'Spend', 'Spin', 'Spit', 'Steal', 'Stink', 'Swear', 'Sweat', 'Sweep', 'Swim', 'Take', 'Teach', 'Tell', 'Think', 'Throw', 'Tread', 'Understand', 'Wear', 'Wet', 'Win', 'Write'];
   var verbosPasadoSimple = ['Arose', 'Awoke', 'Was', 'Beat', 'Became', 'Began', 'Bet', 'Bit', 'Blew', 'Broke', 'Brought', 'Built', 'Bought', 'Caught', 'Chose', 'Came', 'Cost', 'Cut', 'Dug', 'Did', 'Drew', 'Dreamt', 'Drank', 'Drove', 'Ate', 'Fell', 'Fed', 'Felt', 'Fought', 'Found', 'Flew', 'Forgot', 'Forgave', 'Froze', 'Got', 'Gave', 'Went', 'Grew', 'Had', 'Heard', 'Hid', 'Hit', 'Hurt', 'Kept', 'Knelt', 'Knew', 'Leant', 'Learnt', 'Left', 'Lent', 'Let', 'Lay', 'Lit', 'Lost', 'Made', 'Meant', 'Met', 'Paid', 'Pled', 'Put', 'Quit', 'Read', 'Rode', 'Rose', 'Ran', 'Said', 'Saw', 'Sought', 'Sold', 'Sent', 'Set', 'Shook', 'Shone', 'Shot', 'Shut', 'Sang', 'Slept', 'Smelt', 'Spoke', 'Spelt', 'Spent', 'Spun', 'Spat', 'Stole', 'Stank', 'Swore', 'Sweat', 'Swept', 'Swam', 'Took', 'Taught', 'Told', 'Thought', 'Threw', 'Trod', 'Understood', 'Wore', 'Wet', 'Won', 'Wrote'];
   var verbosPasadoParticipio = ['Arisen', 'Awoken', 'Been', 'Beaten', 'Become', 'Begun', 'Bet', 'Bitten', 'Blown', 'Broken', 'Brought', 'Built', 'Bought', 'Caught', 'Chosen', 'Come', 'Cost', 'Cut', 'Dug', 'Done', 'Drawn', 'Dreamt', 'Drunk', 'Driven', 'Eaten', 'Fallen', 'Fed', 'Felt', 'Fought', 'Found', 'Flown', 'Forgotten', 'Forgiven', 'Frozen', 'Gotten', 'Given', 'Gone', 'Grown', 'Had', 'Heard', 'Hidden', 'Hit', 'Hurt', 'Kept', 'Knelt', 'Known', 'Leant', 'Learnt', 'Left', 'Lent', 'Let', 'Lain', 'Lit', 'Lost', 'Made', 'Meant', 'Met', 'Paid', 'Pled', 'Put', 'Quit', 'Read', 'Ridden', 'Risen', 'Run', 'Said', 'Seen', 'Sought', 'Sold', 'Sent', 'Set', 'Shaken', 'Shone', 'Shot', 'Shut', 'Sung', 'Slept', 'Smelt', 'Spoken', 'Spelt', 'Spent', 'Spun', 'Spat', 'Stolen', 'Stunk', 'Sworn', 'Sweat', 'Swept', 'Swum', 'Taken', 'Taught', 'Told', 'Thought', 'Thrown', 'Trodden', 'Understood', 'Worn', 'Wet', 'Won', 'Written'];
-  var verbosTraduccion = ['Surgir', 'Despertar', 'Ser', 'Golpear', 'Convertir', 'Empezar', 'Apostar', 'Morder', 'Soplar', 'Romper', 'Traer', 'Construir', 'Comprar', 'Agarrar', 'Elegir', 'Venir', 'Costar', 'Cortar', 'Cavar', 'Hacer', 'Dibujar', 'Soñar', 'Beber', 'Conducir', 'Comer', 'Caer', 'Alimentar', 'Sentir', 'Pelear', 'Encontrar', 'Volar', 'Olvidar', 'Perdonar', 'Congelar', 'Conseguir', 'Dar', 'Ir', 'Crecer', 'Tener', 'Escuchar', 'Esconder', 'Golpear', 'Lastimar', 'Mantener', 'Arrodillar', 'Saber', 'Apoyar', 'Aprender', 'Abandonar', 'Prestar', 'Dejar', 'Estar acostado', 'Encender', 'Perder', 'Hacer', 'Significar', 'Conocer', 'Pagar', 'Alegar', 'Poner', 'Abandonar', 'Leer', 'Montar', 'Levantar', 'Correr', 'Decir', 'Ver', 'Buscar', 'Vender', 'Enviar', 'Colocar', 'Sacudir', 'Brillar', 'Disparar', 'Cerrar', 'Cantar', 'Dormir', 'Oler', 'Hablar', 'Deletrear', 'Gastar', 'Girar', 'Escupir', 'Robar', 'Apestar', 'Jurar', 'Sudar', 'Barrer', 'Nadar', 'Tomar', 'Enseñar', 'Decir', 'Pensar', 'Tirar', 'Pisar', 'Comprender', 'Vestir', 'Mojar', 'Ganar', 'Escribir'];
+  var verbosTraduccionBackup = ['Surgir', 'Despertar', 'Ser', 'Golpear', 'Convertir', 'Empezar', 'Apostar', 'Morder', 'Soplar', 'Romper', 'Traer', 'Construir', 'Comprar', 'Agarrar', 'Elegir', 'Venir', 'Costar', 'Cortar', 'Cavar', 'Hacer', 'Dibujar', 'Soñar', 'Beber', 'Conducir', 'Comer', 'Caer', 'Alimentar', 'Sentir', 'Pelear', 'Encontrar', 'Volar', 'Olvidar', 'Perdonar', 'Congelar', 'Conseguir', 'Dar', 'Ir', 'Crecer', 'Tener', 'Escuchar', 'Esconder', 'Golpear', 'Lastimar', 'Mantener', 'Arrodillar', 'Saber', 'Apoyar', 'Aprender', 'Abandonar', 'Prestar', 'Dejar', 'Estar acostado', 'Encender', 'Perder', 'Hacer', 'Significar', 'Conocer', 'Pagar', 'Alegar', 'Poner', 'Abandonar', 'Leer', 'Montar', 'Levantar', 'Correr', 'Decir', 'Ver', 'Buscar', 'Vender', 'Enviar', 'Colocar', 'Sacudir', 'Brillar', 'Disparar', 'Cerrar', 'Cantar', 'Dormir', 'Oler', 'Hablar', 'Deletrear', 'Gastar', 'Girar', 'Escupir', 'Robar', 'Apestar', 'Jurar', 'Sudar', 'Barrer', 'Nadar', 'Tomar', 'Enseñar', 'Decir', 'Pensar', 'Tirar', 'Pisar', 'Comprender', 'Vestir', 'Mojar', 'Ganar', 'Escribir'];
+  var verbosTraduccion = ['Surgir', 'Despertar', 'Ser', 'Golpear', 'Convertir', 'Empezar', 'Apostar', 'Morder', 'Soplar', 'Romper', 'Traer', 'Construir', 'Comprar', 'Agarrar', ['Elegir', 'Escoger'], 'Venir', 'Costar', 'Cortar', 'Cavar', 'Hacer', 'Dibujar', 'Soñar', 'Beber', 'Conducir', 'Comer', 'Caer', 'Alimentar', 'Sentir', 'Pelear', 'Encontrar', 'Volar', 'Olvidar', 'Perdonar', 'Congelar', 'Conseguir', 'Dar', 'Ir', 'Crecer', 'Tener', 'Escuchar', ['Esconder','Ocultar'], 'Golpear', 'Lastimar', 'Mantener', 'Arrodillar', 'Saber', 'Apoyar', 'Aprender', 'Abandonar', 'Prestar', 'Dejar', 'Estar acostado', 'Encender', 'Perder', 'Hacer', 'Significar', 'Conocer', 'Pagar', 'Alegar', 'Poner', 'Abandonar', 'Leer', 'Montar', 'Levantar', 'Correr', 'Decir', 'Ver', 'Buscar', 'Vender', 'Enviar', 'Colocar', 'Sacudir', 'Brillar', 'Disparar', 'Cerrar', 'Cantar', 'Dormir', 'Oler', 'Hablar', 'Deletrear', 'Gastar', 'Girar', 'Escupir', 'Robar', 'Apestar', 'Jurar', 'Sudar', 'Barrer', 'Nadar', 'Tomar', 'Enseñar', 'Decir', 'Pensar', 'Tirar', 'Pisar', 'Comprender', 'Vestir', 'Mojar', 'Ganar', 'Escribir'];
 
   var verificarVerboPasado = 'vacio';
   var verificarVerboParticipio = 'vacio';
   var verificarVerboTraduccion = 'vacio';
+  var verificarVerboTraduccionM = 'vacio';
 
   var verbosnuevos = [];
   var verbosPasadosSimplesnuevos = [];
@@ -93,10 +95,9 @@ $(document).ready(function () {
 
   function verbosfunction() {
 
-    valorescrito = $('.input').val();
-    valorescrito2 = $('.input2').val();
-    valorescrito3 = $('.input3').val();
-
+    valorescrito = $.trim($('.input').val());
+    valorescrito2 = $.trim($('.input2').val());
+    valorescrito3 = $.trim($('.input3').val());
 
     if (valorescrito == 'vacio') {
       $('.alerta').hide();
@@ -106,10 +107,9 @@ $(document).ready(function () {
 
     //validar(numero, valorescrito, valorescrito2, valorescrito3);
 
-
-    if ($.trim(valorescrito.toUpperCase()) == verificarVerboPasado.toUpperCase() &&
-      $.trim(valorescrito2.toUpperCase()) == verificarVerboParticipio.toUpperCase() &&
-      $.trim(valorescrito3.toUpperCase()) == verificarVerboTraduccion.toUpperCase()) {
+  if (verificarVerboPasado.includes(valorescrito.toUpperCase()) &&
+      verificarVerboParticipio.includes(valorescrito2.toUpperCase()) &&
+      verificarVerboTraduccion.includes(valorescrito3.toUpperCase())) {
       $('.alerta').text('Correcto');
       $('.alerta').addClass('alert-success');
       $('.alerta').removeClass('alert-danger');
@@ -123,23 +123,31 @@ $(document).ready(function () {
       estado = false;
     }
 
-    if ($.trim(valorescrito.toUpperCase()) == verificarVerboPasado.toUpperCase()) {
+    if (verificarVerboPasado.includes(valorescrito.toUpperCase())) {
       $('.i-verbo-pasado-simple').text(valorescrito);
-    } else if (valorescrito != verificarVerboPasado) {
-      $('.i-verbo-pasado-simple').text(valorescrito + `/ ${verificarVerboPasado}`);
+    } else if (!verificarVerboPasado.includes(valorescrito.toUpperCase())) {
+      $('.i-verbo-pasado-simple').text(valorescrito + `/ ${verificarVerboPasado.toLowerCase()}`);
     }
 
-    if ($.trim(valorescrito2.toUpperCase()) == verificarVerboParticipio.toUpperCase()) {
+    if (verificarVerboParticipio.includes(valorescrito2.toUpperCase())) {
       $('.i-verbo-pasado-participio').text(valorescrito2);
-    } else if (valorescrito2 != verificarVerboParticipio) {
-      $('.i-verbo-pasado-participio').text(valorescrito2 + `/ ${verificarVerboParticipio}`);
+    } else if (!verificarVerboParticipio.includes(valorescrito2.toUpperCase())) {
+      $('.i-verbo-pasado-participio').text(valorescrito2 + `/ ${verificarVerboParticipio.toLowerCase()}`);
     }
 
-    if ($.trim(valorescrito3.toUpperCase()) == verificarVerboTraduccion.toUpperCase()) {
+    if (verificarVerboTraduccion.includes(valorescrito3.toUpperCase())) {
       $('.i-verbo-traduccion').text(valorescrito3);
-    } else if (valorescrito3 != verificarVerboTraduccion) {
-      $('.i-verbo-traduccion').text(valorescrito3 + `/ ${verificarVerboTraduccion}`);
+    } else if (!verificarVerboTraduccion.includes(valorescrito3.toUpperCase())) {
+      $('.i-verbo-traduccion').text(valorescrito3 + `/ ${verificarVerboTraduccionM}`);
     }
+
+    /*
+    if(Array.isArray(verificarVerboTraduccion)){
+      console.log("es array")
+    }else{
+      console.log("no es array")
+    }
+    */
 
     if (estado == true) {
       contar = contar + 1;
@@ -160,9 +168,23 @@ $(document).ready(function () {
         verbosTraduccionnuevos.push(verbosTraduccion[numeroaleatorio])
       }
 
-      verificarVerboPasado = verbosPasadoSimple[numeroaleatorio];
-      verificarVerboParticipio = verbosPasadoParticipio[numeroaleatorio];
-      verificarVerboTraduccion = verbosTraduccion[numeroaleatorio];
+      verificarVerboPasado = verbosPasadoSimple[numeroaleatorio].toUpperCase();
+      verificarVerboParticipio = verbosPasadoParticipio[numeroaleatorio].toUpperCase();
+      //verificarVerboTraduccion = verbosTraduccion[numeroaleatorio];
+
+      if(Array.isArray(verbosTraduccion[numeroaleatorio])){
+        verificarVerboTraduccion = verbosTraduccion[numeroaleatorio];
+      }else{
+        verificarVerboTraduccion = [verbosTraduccion[numeroaleatorio]];
+      }
+
+      verificarVerboTraduccion = verificarVerboTraduccion.map(function (e) { 
+        return e.toUpperCase()
+      });
+      verificarVerboTraduccionM = verificarVerboTraduccion.map(function (e) { 
+        return e.toLowerCase()
+      });
+
 
       verbos.splice(numeroaleatorio, 1);
       verbosPasadoSimple.splice(numeroaleatorio, 1);
@@ -195,6 +217,30 @@ $(document).ready(function () {
 
 
   }
+
+  /*------------------------------------------------------------------------------------------*/
+
+  /*
+  var palabraUno = 'hola';
+  var arreglos = ['dos', 'tres', ['piso', 'hola'], 'tela']
+
+
+  console.log('holaaaa', arreglos.join())
+
+  arreglos.forEach(x => {
+    if (x.includes('piso')) {
+      console.log(x, 'true')
+    } else if (!x.includes('piso')) {
+      console.log(x, 'false')
+    }
+  })
+
+
+  var arreglos = ['dos', 'tres', ['piso', 'hola'], 'tela']
+  arreglos.toUpper();
+  console.log(arreglos)
+  */
+
 
 });
 
